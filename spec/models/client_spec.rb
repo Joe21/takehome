ad9c86173ee04d796e3abd71ea7837a21828fff5
Joe <joej21@gmail.com>
2025-11-05 14:27:17 -0500

    client model and specs complete

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  subject { build(:client) }
+  subject { build(:client, name:) }
 
-  it '', focus: true do
-    debugger
+  let(:name) { "Acme Co." }
+
+  context 'when name ends with whitespace' do
+    let(:name) { super() + "    "}
+
+    before do
+      subject.name = name
+      subject.validate
+    end
+    
+    it { is_expected.to be_valid }
+
+    it 'strips whitespaces from the name' do
+      expect(subject.name[-1]).not_to eq(' ')
+    end
   end
 
+  context 'when a case-insensitive duplicate already exists' do
+    let(:dupe_client) { create(:client, name: name.downcase) }
+
+    before { dupe_client }
+
+    it { is_expected.to be_invalid }
+  end
 end
