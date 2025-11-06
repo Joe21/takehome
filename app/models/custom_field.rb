359de72c3fd22c7f23ef8050f56f0934b9eae1a0
Joe <joej21@gmail.com>
2025-11-05 19:42:37 -0500

    ensure keys are unique per client and add downcasing at model level

--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -3,12 +3,18 @@ class CustomField < ApplicationRecord
 
   FIELD_TYPES = %w[number freeform enum].freeze
 
+  before_validation :downcase_key, if: -> { key.present? }
+
   validates :label, presence: true
-  validates :key, presence: true, uniqueness: { scope: :client_id }
+  validates :key, presence: true, uniqueness: { scope: :client_id } # clients can have the same key
   validates :field_type, presence: true, inclusion: { in: FIELD_TYPES }
   validate :enum_options_required_if_enum
 
   private
+
+  def downcase_key
+    self.key = key.downcase
+  end
 
   def enum_options_required_if_enum
     return unless field_type == 'enum'
--- spec/models/custom_field_spec.rb
+++ spec/models/custom_field_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe CustomField, type: :model do
-  subject { build(:custom_field) }
+  subject { build(:custom_field, client: create(:client)) }
 
   it { is_expected.to belong_to(:client) }
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:label) }
     it { is_expected.to validate_presence_of(:key) }
-    it { is_expected.to validate_uniqueness_of(:key).scoped_to(:client_id) }
+    it { is_expected.to validate_uniqueness_of(:key).scoped_to(:client_id).case_insensitive }
     it { is_expected.to validate_presence_of(:field_type) }
     it { is_expected.to validate_inclusion_of(:field_type).in_array(%w[number freeform enum]) }
 
@@ -35,6 +35,26 @@ RSpec.describe CustomField, type: :model do
       before { subject.enum_options = [] }
 
       it { is_expected.to be_valid }
+    end
+
+    context 'when another client has the same key' do
+      before do
+        other_client = create(:client)
+        create(:custom_field, client: other_client, key: 'living_room_color')
+        subject.key = 'living_room_color'
+        subject.client = create(:client)
+      end
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when the same client has the same key' do
+      before do
+        create(:custom_field, client: subject.client, key: 'LIVING_ROOM_COLOR')
+        subject.key = 'living_room_color'
+      end
+
+      it { is_expected.to be_invalid }
     end
   end
 end
