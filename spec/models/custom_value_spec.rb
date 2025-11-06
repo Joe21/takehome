7b5bde65295a40ee1c53677fa86951c6dac14385
Joe <joej21@gmail.com>
2025-11-05 20:49:33 -0500

    clean up specs

--- spec/models/custom_value_spec.rb
+++ spec/models/custom_value_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe CustomValue, type: :model do
   let(:building) { create(:building, client: client) }
   
   before do
-    building
     create(:custom_field, client: client)
     create(:custom_field, :enum, client: client)
     create(:custom_field, :number, client: client)
@@ -16,20 +15,20 @@ RSpec.describe CustomValue, type: :model do
   it { is_expected.to belong_to(:building) }
   it { is_expected.to validate_presence_of(:values) }
 
-  describe 'values hash population' do
+  describe '#values' do
     it 'assigns a numeric value for number fields' do
-      number_field = client.custom_fields.find { |f| f.field_type == 'number' && f.active }
-      expect(subject.values[number_field.key]).to be_a(Numeric)
+      number_field = client.custom_fields.find { |f| f.field_type == 'number' }
+      expect(subject.values.fetch(number_field.key)).to be_a(Numeric)
     end
 
     it 'assigns a string value for freeform fields' do
-      freeform_field = client.custom_fields.find { |f| f.field_type == 'freeform' && f.active }
-      expect(subject.values[freeform_field.key]).to be_a(String)
+      freeform_field = client.custom_fields.find { |f| f.field_type == 'freeform' }
+      expect(subject.values.fetch(freeform_field.key)).to be_a(String)
     end
 
     it 'assigns a valid enum value' do
-      enum_field = client.custom_fields.find { |f| f.field_type == 'enum' && f.active }
-      expect(enum_field.enum_options).to include(subject.values[enum_field.key])
+      enum_field = client.custom_fields.find { |f| f.field_type == 'enum' }
+      expect(enum_field.enum_options).to include(subject.values.fetch(enum_field.key))
     end
   end
 end
