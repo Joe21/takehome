aa8969f65245fa9a31e146e368ab8e97c514e0cf
Joe <joej21@gmail.com>
2025-11-06 23:27:12 -0500

    seeds completed, adding some visual output

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,94 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Seed 5 Clients
+clients = ['Alpha Co.', 'Bravo Co.', 'Charlie Co.', 'Delta Co.', 'Echo Co.'].map do |name|
+  client = Client.find_or_create_by!(name:)
+  puts client.inspect
+  puts "\n"
+  client
+end
+puts "------------ CLIENTS SEEDED SUCCESSFULLY ------------\n\n"
 
-['Alpha', 'Bravo', 'Charlie', 'Delta', 'Echo'].each do |name|
-  Client.find_or_create_by!(name:)
+# Seed a small sample of custom fields for each client
+clients.each do |client|
+  case client.name
+  when 'Alpha Co.'
+    custom_fields = client.custom_fields.find_or_create_by!(schema_store: {
+      "num_bathrooms"    => "number",
+      "exterior_material" => "string",
+      "walkway_type"     => ["concrete", "gravel", "asphalt"]
+    })
+    puts custom_fields.inspect
+    puts "\n"
+  when 'Bravo Co.'
+    custom_fields = client.custom_fields.find_or_create_by!(schema_store: {
+      "interior_material" => "string",
+      "bedroom_count"     => "number"
+    })
+    puts custom_fields.inspect
+    puts "\n"
+  when 'Charlie Co.'
+    custom_fields = client.custom_fields.find_or_create_by!(schema_store: {
+      "bedroom_window_type" => ["single", "double", "triple"],
+      "indoor_pool_sqft"    => "number"
+    })
+    puts custom_fields.inspect
+    puts "\n"
+  when 'Delta Co.'
+    custom_fields = client.custom_fields.find_or_create_by!(schema_store: {
+      "num_solar_panels"        => "number",
+      "energy_efficiency_rating" => ["A", "B", "C", "D"]
+    })
+    puts custom_fields.inspect
+    puts "\n"
+  when 'Echo Co.'
+    custom_fields = client.custom_fields.find_or_create_by!(schema_store: {
+      "year_built" => "number",
+      "was_church" => ["yes", "no"]
+    })
+  end
+end
+puts "------------ CUSTOM FIELDS SEEDED SUCCESSFULLY ------------\n\n"
+
+# Seed a small sample of buildings for each client that contain values for the custom fields
+clients.each do |client|
+  puts "Seeding buildings for client: #{client.name}"
+
+  case client.name
+  when 'Alpha Co.'
+    buildings = [
+      { address: "101 Alpha St", zip_code: "10001", state: "NY", custom_field_values: {"num_bathrooms"=>2, "exterior_material"=>"Brick", "walkway_type"=>"concrete"} },
+      { address: "102 Alpha Ave", zip_code: "10002", state: "NY", custom_field_values: {"num_bathrooms"=>3, "exterior_material"=>"Wood", "walkway_type"=>"gravel"} }
+    ]
+  when 'Bravo Co.'
+    buildings = [
+      { address: "201 Bravo St", zip_code: "20001", state: "NJ", custom_field_values: {"interior_material"=>"Oak", "bedroom_count"=>4} },
+      { address: "202 Bravo Ave", zip_code: "20002", state: "NJ", custom_field_values: {"interior_material"=>"Pine", "bedroom_count"=>3} }
+    ]
+  when 'Charlie Co.'
+    buildings = [
+      { address: "301 Charlie Rd", zip_code: "30001", state: "GA", custom_field_values: {"bedroom_window_type"=>"double", "indoor_pool_sqft"=>500} },
+      { address: "302 Charlie Blvd", zip_code: "30002", state: "GA", custom_field_values: {"bedroom_window_type"=>"triple", "indoor_pool_sqft"=>750} }
+    ]
+  when 'Delta Co.'
+    buildings = [
+      { address: "401 Delta Dr", zip_code: "40001", state: "IL", custom_field_values: {"num_solar_panels"=>20, "energy_efficiency_rating"=>"A"} },
+      { address: "402 Delta Ln", zip_code: "40002", state: "IL", custom_field_values: {"num_solar_panels"=>15, "energy_efficiency_rating"=>"B"} }
+    ]
+  when 'Echo Co.'
+    buildings = [
+      { address: "501 Echo St", zip_code: "50001", state: "CA", custom_field_values: {"year_built"=>1920, "was_church"=>"yes"} },
+      { address: "502 Echo Ave", zip_code: "50002", state: "CA", custom_field_values: {"year_built"=>1985, "was_church"=>"no"} }
+    ]
+  end
+
+  buildings.each do |attrs|
+    building = client.buildings.find_or_create_by!(address: attrs[:address]) do |b|
+      b.zip_code = attrs[:zip_code]
+      b.state = attrs[:state]
+      b.custom_field_values = attrs[:custom_field_values]
+    end
+    puts "Seeded building: #{building.address} with custom fields: #{building.custom_field_values.inspect}\n"
+  end
+  puts "-------------------------------------------------------"
 end
 
-
+puts "\n------------ BUILDINGS SEEDED SUCCESSFULLY ------------\n"
