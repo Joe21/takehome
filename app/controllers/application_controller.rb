72e2279ae4d2b1305d8f61351bb603be9ffec991
Joe <joej21@gmail.com>
2025-11-08 08:31:40 -0500

    disable csrf to allow local testing with bruno, need to update status_code on validation error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  # DISABLED AND SWITCHED TO ActionController::API to allow bruno/postman
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
 end
